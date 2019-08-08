b67ba10ed53b4a003bca66027410644071f3be05
Cuttlerat <avkioller@gmail.com>
2019-08-08 13:44:43 +0300

    Add commands to router

--- bot.pl
+++ bot.pl
@@ -27,31 +27,58 @@ update_offset(Message) :-
     NewOffset #= Message.get(update_id) + 1,
     http_post(URL, form_data([offset = NewOffset]), _, []).
 
-get_commands([], []) :- !.
-get_commands([Message|Tail], [Message|OutputTail]) :-
+is_command(Message) :-
     [Entity|_] = Message.get(message).get(entities),
-    Entity.get(type) = "bot_command",
-    update_offset(Message),
-    get_commands(Tail, OutputTail),
-    !.
-get_commands([_|Tail], Output) :-
-    get_commands(Tail, Output).
+    Entity.get(type) = "bot_command".
 
-router([]) :- !.
-router([Message|Tail]) :-
+command_to_name(Command, Name) :-
+    atomic_concat("telegram_command_", Command, Name).
+
+text_to_command(Message, Command, Args) :-
+    split_string(Message, " ", "", [CommandTmp|Args]),
+    split_string(CommandTmp, "@", "/", [Command|_]).
+
+construct_functor(Name, [], Functor) :-
+    functor(Functor, Name, 0), !.
+construct_functor(Name, Args, Functor) :-
+    length(Args, Arity),
+    functor(Functor, Name, Arity),
+    construct_functor_(Name, Args, Functor, Arity, 0).
+
+construct_functor_(_, [], _, X, X) :- !.
+construct_functor_(Name, [Arg|Args], Functor, Arity, ArgNumber) :-
+    ArgNumber1 #= ArgNumber + 1,
+    arg(ArgNumber1, Functor, Arg),
+    construct_functor_(Name, Args, Functor, Arity, ArgNumber1).
+
+log_print(ChatID, Command) :-
+    format('[~d]: ~s~n', [ChatID, Command]).
+
+router(Message) :-
     ChatID = Message.get(message).get(chat).get(id),
-    Command = Message.get(message).get(text),
+    text_to_command(Message.get(message).get(text), Command, Args),
+    command_to_name(Command, Name),
+    construct_functor(Name, [Args, Text], Functor),
+    consult(commands),
+    current_predicate(_, Functor),
+    call(Functor),
     url("sendMessage", URL),
-    string_concat(Command, " is not implemented yet.", Text),
     http_post(URL, form_data([ text = Text,
                                chat_id = ChatID ]), _, []),
-    format('[~d]: ~s~n', [ChatID, Command]),
-    router(Tail).
+    log_print(ChatID, Command),
+    !.
+router(_).
 
-process_messages() :-
+process_messages :-
     get_updates(Data),
-    get_commands(Data.get(result), Commands),
-    router(Commands),
-    process_messages().
+    include(is_command, Data.get(result), Commands),
+    maplist(router, Commands),
+    (
+        last(Data.get(result), Last),
+        update_offset(Last)
+    ;
+        true
+    ),
+    process_messages.
 
-%?- process_messages().
+%?- process_messages.
---
+++ commands.pl
@@ -0,0 +1,1 @@
+telegram_command_info(_, "OMG, I work somehow! Please star me: https://github.com/Cuttlerat/prolog_bot").
