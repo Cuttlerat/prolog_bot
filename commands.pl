f1057e66bd6d68fd32501c14d923f1b7a32306f8
Cuttlerat <avkioller@gmail.com>
2019-08-08 15:08:53 +0300

    Add date command

--- commands.pl
+++ commands.pl
@@ -1,1 +1,15 @@
+
 telegram_command_info(_, "OMG, I work somehow! Please star me: https://github.com/Cuttlerat/prolog_bot").
+
+telegram_command_date([], Date) :-
+    get_time(TimeStamp),
+    get_date(TimeStamp, Date).
+
+telegram_command_date([TimeStampStr], Date) :-
+    number_string(TimeStamp, TimeStampStr),
+    get_date(TimeStamp, Date).
+
+get_date(TimeStamp, Date) :-
+    use_module(library(http/http_header)),
+    http_timestamp(TimeStamp, Atom),
+    atom_string(Atom, Date).
