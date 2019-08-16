fb51b2c3c96a6c98d2998bf480a497af2839c8d3
Cuttlerat <avkioller@gmail.com>
2019-08-16 18:25:40 +0300

    Add strip for pingers

--- ping_utils.pl
+++ ping_utils.pl
@@ -3,8 +3,16 @@ get_ping_match(Message, Username) :-
     string_lower(Message.get(message).get(text), Text),
     consult(pingers),
     ping_phrase(PingPhrase),
-    split_string(Text, " ", " ", TextSplitted),
+    strip_chars(Text, [",", ".", ";", ":", "!", "?"], TextStripped),
+    split_string(TextStripped, " ", " ", TextSplitted),
     member(PingPhrase, TextSplitted),
     ping_match(ChatID, Username, Match),
     Message.get(message).get(chat).get(id) = ChatID,
     member(Match, TextSplitted).
+
+strip_chars(X, [], X) :- !.
+strip_chars(String, [H|Exclude], Result) :-
+    split_string(String, H, "", StrippedList),
+    atomics_to_string(StrippedList, Result1),
+    strip_chars(Result1, Exclude, Result).
+
