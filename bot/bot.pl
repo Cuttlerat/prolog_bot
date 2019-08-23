80c5ce768d14b90f3e10e495647cb83a8c04fd23
Cuttlerat <avkioller@gmail.com>
2019-08-23 18:24:47 +0300

    Fix issue with update

--- bot/bot.pl
+++ bot/bot.pl
@@ -87,7 +87,8 @@ router(command, Message) :-
         Message.get(message).get(message_id),
         Message.get(message).get(chat).get(id)),
     atomics_to_string([Command|Args], " ", Log),
-    log_print(Message, Log).
+    log_print(Message, Log),
+    !.
 
 router(ping, _, []) :- !.
 router(ping, Message, Usernames) :-
@@ -103,19 +104,20 @@ router(ping, Message, Usernames) :-
 
 process_message(Message) :-
     update_offset(Message),
-    fail.
-
-process_message(Message) :-
     findall(Username, get_ping_match(Message, Username), Usernames),
     list_to_set(Usernames, UsernamesUniq),
-    router(ping, Message, UsernamesUniq).
+    router(ping, Message, UsernamesUniq),
+    !.
 
 process_message(Message) :-
+    update_offset(Message),
     is_command(Message),
     router(command, Message),
     !.
 
-process_message(_).
+process_message(Message) :-
+    update_offset(Message),
+    !.
 
 main :-
     repeat,
